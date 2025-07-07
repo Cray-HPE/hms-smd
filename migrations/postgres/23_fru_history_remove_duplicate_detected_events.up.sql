7d1eb646997d8f3b9a92b914b0082ccd9c20143f
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-07-07 10:47:09 -0700

    CASMHMS-6568: Remove extraneous AND

--- migrations/postgres/23_fru_history_remove_duplicate_detected_events.up.sql
+++ migrations/postgres/23_fru_history_remove_duplicate_detected_events.up.sql
@@ -43,7 +43,7 @@ BEGIN
     FOR unique_id,unique_fru_id IN SELECT distinct id,fru_id FROM hwinv_hist LOOP
         SELECT * INTO fru_event1 FROM hwinv_hist WHERE id = unique_id AND fru_id = unique_fru_id ORDER BY timestamp ASC LIMIT 1;
         FOR fru_event2 IN SELECT * FROM hwinv_hist WHERE id = unique_id AND fru_id = unique_fru_id AND timestamp != fru_event1.timestamp ORDER BY timestamp ASC LOOP
-            IF AND fru_event1.event_type = 'Detected' AND fru_event1.event_type = fru_event2.event_type THEN
+            IF fru_event1.event_type = 'Detected' AND fru_event1.event_type = fru_event2.event_type THEN
                 DELETE FROM hwinv_hist WHERE id = fru_event2.id AND fru_id = fru_event2.fru_id AND timestamp = fru_event2.timestamp;
             ELSE
                 fru_event1 = fru_event2;
