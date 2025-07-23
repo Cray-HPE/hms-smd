be70629d824d6230909068a62eec071e0dbdf387
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-07-23 12:50:52 -0700

    CASMHMS-6578: Updates from running on tyr

--- scripts/fru_history_remove_duplicate_detected_events.sh
+++ scripts/fru_history_remove_duplicate_detected_events.sh
@@ -40,86 +40,94 @@ set -eo pipefail
 DB_NAME="hmsds"
 DB_USER="postgres"
 
+# Determine the SMD postgres leader
+
+echo "Determining the postgres leader..."
+
+POSTGRES_LEADER=$(kubectl exec cray-smd-postgres-0 -n services -c postgres -t -- patronictl list -f json | jq -r '.[] | select(.Role == "Leader").Member')
+
+echo "The SMD postgres leader is $POSTGRES_LEADER"
+
 # Capture and print sizes before pruning and vacuuming
 
-starting_stats_output=$(
-psql "dbname=$DB_NAME user=$DB_USER" 2>&1 <<'SQL'
-DO $$
-DECLARE
-    tbl_size_before    bigint := 0;   /* hwinv_history size before pruning */
-    db_size_before     bigint := 0;   /* DB size before pruning */
-BEGIN
-    SELECT pg_total_relation_size('hwinv_hist') INTO tbl_size_before;
-    SELECT pg_database_size(current_database()) INTO db_size_before;
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "
+	psql \"dbname=$DB_NAME user=$DB_USER\" -c \"
+	DO \\\$\$
+	DECLARE
+		tbl_size_before    bigint := 0;   /* hwinv_history size before pruning */
+		db_size_before     bigint := 0;   /* DB size before pruning */
+	BEGIN
+		SELECT pg_total_relation_size('hwinv_hist') INTO tbl_size_before;
+		SELECT pg_database_size(current_database()) INTO db_size_before;
 
-    RAISE NOTICE 'hwinv_history table size before pruning:  % mb', tbl_size_before / 1024 / 1024;
-    RAISE NOTICE 'Database size before pruning:             % mb', db_size_before / 1024 / 1024;
-END;
-$$ LANGUAGE plpgsql;
-SQL
-)
+		RAISE NOTICE 'hwinv_history table size before pruning:  % mb', tbl_size_before / 1024 / 1024;
+		RAISE NOTICE 'Database size before pruning:             % mb', db_size_before / 1024 / 1024;
+	END;
+	\\\$\$ LANGUAGE plpgsql;\"
+"
 
-echo "$starting_stats_output"
-
-echo ''
-echo 'Operations may take considerable time - please do not interrupt'
-echo ''
+echo ""
+echo "Operations may take considerable time - please do not interrupt"
 
 # Creating this temporary index speeds up execution by several orders of
 # magnitude. We'll drop it after pruning is complete.
 
-echo 'Creating temporary index on hwinv_hist table...'
+echo ""
+echo "Creating temporary index on hwinv_hist table..."
 
-psql "dbname=$DB_NAME user=$DB_USER" -c \
-  "CREATE INDEX IF NOT EXISTS hwinvhist_id_ts_idx ON hwinv_hist (id, "timestamp");"
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "
+	psql \"dbname=$DB_NAME user=$DB_USER\" -c \
+		\"CREATE INDEX IF NOT EXISTS hwinvhist_id_ts_idx ON hwinv_hist (id, \"timestamp\");\"
+"
 
 if [[ $? -ne 0 ]]; then
-  echo "Error creating temporary timestamp index" >&2
-  exit 1
+	echo "Error creating temporary timestamp index" >&2
+	exit 1
 fi
 
 # Run the pruning logic
 
-echo 'Pruning hwinv_hist table... '
+echo ""
+echo "Pruning hwinv_hist table... "
 
-pruning_output=$(
-psql "dbname=$DB_NAME user=$DB_USER" 2>&1 <<'SQL'
-  WITH ordered AS (
-    SELECT ctid, id, "timestamp", event_type,
-           LAG(event_type) OVER (PARTITION BY id ORDER BY "timestamp") AS prev_type
-    FROM hwinv_hist
-    WHERE id IN (
-      SELECT loc.id
-      FROM hwinv_by_loc loc
-      WHERE loc.type IN ('Processor', 'NodeAccel')
-    )
-  ),
-  dups AS (
-    SELECT ctid
-    FROM ordered
-    WHERE event_type = 'Detected' AND prev_type = 'Detected'
-  )
-  DELETE FROM hwinv_hist
-  WHERE ctid IN (SELECT ctid FROM dups);
-SQL
-)
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "
+	psql \"dbname=$DB_NAME user=$DB_USER\" -c \"
+	WITH ordered AS (
+		SELECT ctid, id, \"timestamp\", event_type,
+			LAG(event_type) OVER (PARTITION BY id ORDER BY \"timestamp\") AS prev_type
+		FROM hwinv_hist
+		WHERE id IN (
+			SELECT loc.id
+			FROM hwinv_by_loc loc
+			WHERE loc.type IN ('Processor', 'NodeAccel')
+		)
+	),
+	dups AS (
+		SELECT ctid
+		FROM ordered
+		WHERE event_type = 'Detected' AND prev_type = 'Detected'
+	)
+	DELETE FROM hwinv_hist
+	WHERE ctid IN (SELECT ctid FROM dups);\"
+"
 
 if [[ $? -ne 0 ]]; then
-  echo "Error executing SQL command" >&2
-  exit 1
+	echo "Error executing SQL command" >&2
+	exit 1
 fi
-
-echo "$pruning_output"
 
 # Now drop the index to free up associated resources
 
-echo 'Dropping temporary index on hwinv_hist table...'
+echo ""
+echo "Dropping temporary index on hwinv_hist table..."
 
-psql "dbname=$DB_NAME user=$DB_USER" -c "DROP INDEX IF EXISTS hwinvhist_id_ts_idx;"
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "
+	psql \"dbname=$DB_NAME user=$DB_USER\" -c \"DROP INDEX IF EXISTS hwinvhist_id_ts_idx;\"
+"
 
 if [[ $? -ne 0 ]]; then
-  echo "Error dropping temporary timestamp index" >&2
-  exit 1
+	echo "Error dropping temporary timestamp index" >&2
+	exit 1
 fi
 
 # The pruning logic above removed a large part of the hwinv_hist table. This
@@ -139,30 +147,34 @@ fi
 #
 # So, run a full vacuum
 
+echo ""
 echo "Running VACUUM FULL on hwinv_hist table to reclaim disk space..."
 
-psql "dbname=$DB_NAME user=$DB_USER" -c "VACUUM FULL hwinv_hist;" || {
-  echo "Error running VACUUM FULL" >&2
-  exit 1
-}
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "
+	psql \"dbname=$DB_NAME user=$DB_USER\" -c \"VACUUM FULL hwinv_hist;\"
+"
+
+if [[ $? -ne 0 ]]; then
+	echo "Error running VACUUM FULL" >&2
+	exit 1
+fi
 
 # Capture and print sizes after pruning and vacuuming
 
-ending_stats_output=$(
-psql "dbname=$DB_NAME user=$DB_USER" 2>&1 <<'SQL'
-DO $$
-DECLARE
-    tbl_size_after    bigint := 0;   /* hwinv_history size after pruning */
-    db_size_after     bigint := 0;   /* DB size after pruning */
-BEGIN
-    SELECT pg_total_relation_size('hwinv_hist') INTO tbl_size_after;
-    SELECT pg_database_size(current_database()) INTO db_size_after;
+echo ""
 
-    RAISE NOTICE 'hwinv_history table size after pruning:  % mb', tbl_size_after / 1024 / 1024;
-    RAISE NOTICE 'Database size after pruning:             % mb', db_size_after / 1024 / 1024;
-END;
-$$ LANGUAGE plpgsql;
-SQL
-)
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "
+	psql \"dbname=$DB_NAME user=$DB_USER\" -c \"
+	DO \\\$\$
+	DECLARE
+		tbl_size_after    bigint := 0;   /* hwinv_history size after pruning */
+		db_size_after     bigint := 0;   /* DB size after pruning */
+	BEGIN
+		SELECT pg_total_relation_size('hwinv_hist') INTO tbl_size_after;
+		SELECT pg_database_size(current_database()) INTO db_size_after;
 
-echo "$ending_stats_output"
+		RAISE NOTICE 'hwinv_history table size after pruning:  % mb', tbl_size_after / 1024 / 1024;
+		RAISE NOTICE 'Database size after pruning:             % mb', db_size_after / 1024 / 1024;
+	END;
+	\\\$\$ LANGUAGE plpgsql;\"
+"
