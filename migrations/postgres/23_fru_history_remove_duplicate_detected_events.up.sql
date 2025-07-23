5116da605c09e100a6adc1677d049a0e81675ffd
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-07-23 12:56:55 -0700

    CASMHMS-6578: Forgot to add new files

---
+++ migrations/postgres/23_fru_history_remove_duplicate_detected_events.up.sql
@@ -0,0 +1,70 @@
+/*
+ * MIT License
+ *
+ * (C) Copyright [2025] Hewlett Packard Enterprise Development LP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+-- Removes duplicate "Detected" events from the hardware history table
+
+CREATE OR REPLACE FUNCTION hwinv_hist_remove_duplicate_detected_events()
+RETURNS VOID AS $$
+BEGIN
+    -- Creating this temporary index speeds up execution by several orders of
+    -- magnitude. We'll drop it after pruning is complete.
+
+    CREATE INDEX IF NOT EXISTS hwinvhist_id_ts_idx ON hwinv_hist (id, "timestamp");
+
+    -- Run the pruning logic
+
+    WITH ordered AS (
+        SELECT ctid, id, "timestamp", event_type,
+               LAG(event_type) OVER (PARTITION BY id ORDER BY "timestamp") AS prev_type
+        FROM hwinv_hist
+        WHERE id IN (
+            SELECT loc.id
+            FROM hwinv_by_loc loc
+            WHERE loc.type IN ('Processor', 'NodeAccel')
+        )
+    ),
+    dups AS (
+        SELECT ctid
+        FROM ordered
+        WHERE event_type = 'Detected' AND prev_type = 'Detected'
+    )
+    DELETE FROM hwinv_hist
+    WHERE ctid IN (SELECT ctid FROM dups);
+
+    -- Drop the temporary index to free up associated resources
+
+    DROP INDEX IF EXISTS hwinvhist_id_ts_idx;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Execute the pruning function
+
+SELECT hwinv_hist_remove_duplicate_detected_events();
+
+-- A full vacuum must be run to reclaim space but cannot run from a migration.
+-- The cray-smd-init service will run it manually after the migration completes.
+
+-- Bump the schema version
+insert into system values(0, 21, '{}'::JSON)
+    on conflict(id) do update set schema_version=21;
