9f2890bf0efff125c0e79c0d486f25da320bc383
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-07-31 13:02:31 -0700

    Add migration to prune duplicate "Detected" events from CPUs and GPUs (#179)
    - Added migration to prune duplicate "Detected" events from CPUs and GPUs
    - Internal tracking ticket: CASMHMS-6578

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [2.32.2] - 2025-07-23
+## [2.32.3] - 2025-07-31
+
+### Fixed
+
+- Added migration to prune duplicate "Detected" events from CPUs and GPUs
+- Internal tracking ticket: CASMHMS-6578
+
+## [2.32.2] - 2025-07-17
 
 ### Fixed
 
--- scripts/hwinv_by_loc_backup.sh
+++ scripts/hwinv_by_loc_backup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#! /usr/bin/env bash
 #
 # MIT License
 #
-# (C) Copyright [2025] Hewlett Packard Enterprise Development LP
+# (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -21,6 +21,31 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-#
 
-docker compose -f docker-compose.yaml up -d --build
+# This script backs up the hwinv_by_loc table in the SMD postgres database.
+
+set -eo pipefail
+
+DB_NAME="hmsds"
+DB_USER="postgres"
+DB_TABLE="hwinv_by_loc"
+
+BACKUP_FILE="smd_hwinv_by_loc_table_backup-$(date +"%m%d%Y-%H:%M:%S").sql"
+
+# Determine the SMD postgres leader
+
+echo "Determining the postgres leader..."
+
+POSTGRES_LEADER=$(kubectl exec cray-smd-postgres-0 -n services -c postgres -t -- patronictl list -f json | jq -r '.[] | select(.Role == "Leader").Member')
+
+echo "The SMD postgres leader is $POSTGRES_LEADER"
+
+# Dump the contents of the hwinv_by_loc table.  We use --clean so that
+# the dump file can be used to replace the table contents if necessary
+# during a restore operation in the future if necessary
+
+echo "Using pg_dump to dump the $DB_TABLE table..."
+
+kubectl -n services exec "$POSTGRES_LEADER" -c postgres -it -- bash -c "pg_dump -U $DB_USER -d $DB_NAME -t $DB_TABLE --clean" > "$BACKUP_FILE"
+
+echo "Dump complete. Dump file is: $BACKUP_FILE"
